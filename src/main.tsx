298b736e0b7a98119cf782fb9cfb0d65e7d9039b
Jeferson19283746 <jeferson02oliveira02@gmail.com>
2026-09-07 09:05:30 -0300

    style: load product showcase styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 import './v2.css'
+import './product.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
